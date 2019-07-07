2c8e4975994b7045f92a2b865d6f0b7921994d35
laia89 <laiacy234@gmail.com>
2019-07-08 00:36:29 +0200

    create a new repository for the final project called Babyboom for the SkylabCoders bootcamp

---
+++ baby-app/app/src/index.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+)
+registerServiceWorker()
